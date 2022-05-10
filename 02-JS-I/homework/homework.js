af61bec62d0f3c6183abddb83711c7b450f70927
MartinFarro <102565755+MartinFarro@users.noreply.github.com>
2022-05-10 16:52:15 -0500

    firts commit

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -242,8 +242,7 @@ function esVocal(letra){
   //Verificar si el usuario ingresó un string de más de un carácter, en ese caso, informarle 
   //que no se puede procesar el dato mediante el mensaje "Dato incorrecto".
   // Si no es vocal, tambien debe devolver "Dato incorrecto".
-  //Escribe tu código aquí
-  //solucionado
+  //Escribe tu código aqui
    if (letra.length > 1){
     return "Dato incorrecto";
    } else{
